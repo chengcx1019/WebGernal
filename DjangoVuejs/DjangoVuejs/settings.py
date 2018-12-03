95cd9903533e97f0443940b5b0ef76a9333160fc
changxincheng <chengcx1019@gmail.com>
2018-12-03 22:12:25 +0800

    allowhost

--- DjangoVuejs/DjangoVuejs/settings.py
+++ DjangoVuejs/DjangoVuejs/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'xi1dq=*h_lwo4^9_h)uw&tyd5%zyi*o9&k13@(6(duhdrdn7b6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
